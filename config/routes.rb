75f4e19aa44f1df5bed4f5637acbeb79322d622b
leon-siqueira <leon.siqueir4@gmail.com>
2022-02-28 16:51:58 -0300

    views and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lists, only: [:index, :show, :create, :new, :edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+    # resources :movies, only: [:new, :create]
+  end
+
+
+  resources :bookmarks, only: [:destroy]
+  root 'lists#index'
+  # resources :movies, only: [:destroy]
 end
